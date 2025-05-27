8a41d9807e2e05cc8f39a60ffdefe76891275c9d
playkoround <seasouphan@gmail.com>
2025-05-27 17:52:30 +0900

    Fix: 서비스에 @RequiredArgsConstructor 추가

--- src/main/java/com/camp/nbcamp6thgitprc/service/BookSaveService.java
+++ src/main/java/com/camp/nbcamp6thgitprc/service/BookSaveService.java
@@ -3,15 +3,14 @@ package com.camp.nbcamp6thgitprc.service;
 import com.camp.nbcamp6thgitprc.dto.SaveBookRequest;
 import com.camp.nbcamp6thgitprc.entity.Book;
 import com.camp.nbcamp6thgitprc.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookSaveService {
     private final BookRepository bookRepository;
 
-    public BookSaveService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     public Book saveBook(SaveBookRequest request) {
         Book book = new Book();
