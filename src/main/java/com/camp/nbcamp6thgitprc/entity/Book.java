1aa23b42a9fb4fa35a8f4ee5569ecca46dc3f045
playkoround <seasouphan@gmail.com>
2025-05-27 17:20:15 +0900

    Feat: 선택한 도서 정보를 DB에 저장하는 기능 구현 (#5)

---
+++ src/main/java/com/camp/nbcamp6thgitprc/entity/Book.java
@@ -0,0 +1,23 @@
+package com.camp.nbcamp6thgitprc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String contents;
+    private String authors;
+    private String publisher;
+    private String thumbnail;
+
+}
