c4203d7abe5804bff5c228011b777cfc521fb1c9
ddagom <mstbright.249@gmail.com>
2025-05-27 17:05:18 +0900

    adoptdragonia

---
+++ src/main/java/com/camp/nbcamp6thgitprc/entity/Book.java
@@ -0,0 +1,21 @@
+package com.camp.nbcamp6thgitprc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+
+@Getter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String contents;
+    private String authors;
+    private String publisher;
+    private String thumbnail;
+}
