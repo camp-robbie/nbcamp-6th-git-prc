1a5cdc72486724183816758f8e728629a61150dc
choyooseok <bluesbeat93@gmail.com>
2025-05-27 17:10:16 +0900

    feat : 사용자가 선택한 도서 DB 저장 기능 구현

---
+++ src/main/java/com/camp/nbcamp6thgitprc/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.camp.nbcamp6thgitprc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.camp.nbcamp6thgitprc.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+	// 커스텀 메서드 필요 시 추가
+}
